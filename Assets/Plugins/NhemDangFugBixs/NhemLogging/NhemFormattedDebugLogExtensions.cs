998bc02a41c1a42ccaeca9ab357755829ca7d29c
NhomNhem <truongduy18.03.03.al@gmail.com>
2026-05-16 19:51:06 +0700

    feat: Integrate Nhem logging extensions and update project configuration for logging

---
+++ Assets/Plugins/NhemDangFugBixs/NhemLogging/NhemFormattedDebugLogExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Cysharp.Text;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace NhemDangFugBixs.NhemLogging
+{
+    public static class NhemFormattedDebugLogExtensions
+    {
+        [Conditional("GR_COMBAT_DEBUG")]
+        public static void CombatDebugFormat<T1, T2, T3>(
+            this INhemLogger logger,
+            string format,
+            T1 arg1,
+            T2 arg2,
+            T3 arg3,
+            Object? context = null,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            logger.Log(ZString.Format(format, arg1, arg2, arg3), context, file, line);
+        }
+
+        [Conditional("GR_MEMORY_DEBUG")]
+        public static void MemoryDebugFormat<T1, T2>(
+            this INhemLogger logger,
+            string format,
+            T1 arg1,
+            T2 arg2,
+            Object? context = null,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            logger.Log(ZString.Format(format, arg1, arg2), context, file, line);
+        }
+    }
+}
---
+++ Assets/Plugins/NhemDangFugBixs/NhemLogging/NhemLogDebugExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace NhemDangFugBixs.NhemLogging
+{
+    public static class NhemLogDebugExtensions
+    {
+        [Conditional("GR_INPUT_DEBUG")]
+        public static void InputDebug(
+            this INhemLogger logger,
+            object? message,
+            Object? context = null,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            logger.Log(message, context, file, line);
+        }
+
+        [Conditional("GR_COMBAT_DEBUG")]
+        public static void CombatDebug(
+            this INhemLogger logger,
+            object? message,
+            Object? context = null,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            logger.Log(message, context, file, line);
+        }
+
+        [Conditional("GR_MEMORY_DEBUG")]
+        public static void MemoryDebug(
+            this INhemLogger logger,
+            object? message,
+            Object? context = null,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            logger.Log(message, context, file, line);
+        }
+
+        [Conditional("GR_ENCOUNTER_DEBUG")]
+        public static void EncounterDebug(
+            this INhemLogger logger,
+            object? message,
+            Object? context = null,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            logger.Log(message, context, file, line);
+        }
+
+        [Conditional("GR_TARGETING_DEBUG")]
+        public static void TargetingDebug(
+            this INhemLogger logger,
+            object? message,
+            Object? context = null,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            logger.Log(message, context, file, line);
+        }
+
+        [Conditional("GR_CAMERA_DEBUG")]
+        public static void CameraDebug(
+            this INhemLogger logger,
+            object? message,
+            Object? context = null,
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int line = 0)
+        {
+            logger.Log(message, context, file, line);
+        }
+    }
+}
